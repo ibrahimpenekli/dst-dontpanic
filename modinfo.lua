bee9db7bf50309d4a3e6eb09bb018bd28d77b611
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2021-08-31 13:09:20 +0300

    Update mod info description

--- modinfo.lua
+++ modinfo.lua
@@ -1,5 +1,20 @@
 name = "Don't Panic"
-description = "Warns you for boss spawn as text!"
+description = [[Alerts players by text against season boss and hound/worm attacks.
+
+Only players near the target player will hear the warning sound from Deerclops and Bearger. So, this mod makes your life easier if you play with a friend next to you has speakers muted.
+
+Supported monsters:
+- Deerclops
+- Bearger
+- Hounds or worms
+
+Options:
+- Off: Disables announcement
+- Default: Alerts only if monster is growling
+- In a day: Alerts both before a day and when monster is growling
+
+On GitHub: https://github.com/ibrahimpenekli/dst-dontpanic
+]]
 author = "H.Ibrahim Penekli (ibrahimpenekli)"
 version = "1.0.0"
 
