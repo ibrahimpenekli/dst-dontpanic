08c67fc698ab4c9d4bb02ea5d7843e0540e73865
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2021-08-31 13:19:54 +0300

    Add steam url

--- modinfo.lua
+++ modinfo.lua
@@ -13,6 +13,7 @@ Options:
 - Default: Alerts only if monster is growling
 - In a day: Alerts both before a day and when monster is growling
 
+On Steam: https://steamcommunity.com/sharedfiles/filedetails/?id=2589904748
 On GitHub: https://github.com/ibrahimpenekli/dst-dontpanic
 ]]
 author = "H.Ibrahim Penekli (ibrahimpenekli)"
